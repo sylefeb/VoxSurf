cfe5f38b55b210228e95edc75622ee2e7ea3782d
Joseph Toombs <38547166+toomb-raider@users.noreply.github.com>
2020-05-31 14:07:10 -0700

    setup.py update

--- setup.py
+++ setup.py
@@ -73,9 +73,10 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+
 setup(
     name='pyvoxsurf',
-    version='1.0.4',
+    version='1.0.6',
     author='Joseph Toombs',
     author_email='jtoombs719@gmail.com',
     url='https://github.com/jttoombs/PyVoxSurf',
@@ -84,6 +85,5 @@ setup(
     long_description_content_type='text/markdown',
     ext_modules=[CMakeExtension('pyvoxsurf')],
     cmdclass=dict(build_ext=CMakeBuild),
-    install_requires=['platform_system=="Windows"'],
     zip_safe=False,
 )
